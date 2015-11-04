e53700a31bcc720112840ee4331f1362564f84a7
met4000 <met4000@mail.com>
2015-11-04 14:38:00 +0800

    Graphics fix

--- resources/pokemon/charmander.cmd
+++ resources/pokemon/charmander.cmd
@@ -7,7 +7,7 @@ if (%pokemode%)==(starter) goto CHOOSING
 
 :CHOOSING
 echo.
-call pokebatch_charmander_graphics.cmd
+call charmander_graphics.cmd
 echo.
 echo      Name: Charmander
 echo      Type: Fire
